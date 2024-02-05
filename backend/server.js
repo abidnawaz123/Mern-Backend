60fc9b5d411010ec668a2fbb938a895b34f81786
AbidNawaz <abidpart2ics@gmail.com>
2024-02-05 18:33:46 +0500

    mern backend

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config()
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+const workoutRouters = require("./routers/workoutModel")
+
+app.use("/api/workouts", workoutRouters)
+
+const mongoose = require("mongoose")
+
+//connect to database 
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {
+        app.listen(process.env.PORT, () => {
+            console.log('Connected to the Database and server is running on port ', process.env.PORT || 4000)
+        })
+    })
+    .catch((error) => {
+        console.log(error)
+    })
